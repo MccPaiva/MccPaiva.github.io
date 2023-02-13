f24905f49318dabf96248150a842f763f3c04f96
MccPaiva <mccpaiva@outlook.com>
2023-02-13 15:19:36 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import logo from "./img/logo.svg";
+import CVPT from "./img/CVPT.pdf";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+          <div className="header-container">
+            <img className="logo" src={logo} alt=""></img>
+            <nav>
+              <ul>
+                <li><a>Home</a></li>
+                <li><a>Projetos</a></li>
+                <li><a>Sobre</a></li>
+              </ul>
+            </nav>
+          </div>
       </header>
+      <div className="container">
+        <section className="landing">
+            <div>
+              <h1>Bem-vindo, meu nome é Matheus Castanheira.</h1>
+              <h2>Eu sou um desenvolvedor web.</h2>
+              <nav className="flex">
+                <ul>
+                  <li><a href="https://github.com/MccPaiva" target="_blank">Github</a></li>
+                  <li><a href="https://www.linkedin.com/in/matheuscastanheira/" target="_blank">Linkedin</a></li>
+                  <li><a href={CVPT} target="_blank">Currículo</a></li>
+                </ul>
+              </nav>
+            </div>
+        </section>
+        <section className="projects">
+          <h1 className="project-title">Projetos</h1>
+          
+          <div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+// 
+//         <iframe
+//           src={CVPT}
+//           frameBorder="0"
+//           scrolling="auto"
+//           height="100%"
+//           width="100%"
+//           >
+//         </iframe>
